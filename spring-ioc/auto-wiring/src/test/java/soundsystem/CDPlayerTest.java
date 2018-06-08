56ddbf669926c5172129d903c9817585af12a9a2
linj2n <linj2n@163.com>
2018-06-08 14:09:15 +0800

    [update]

---
+++ spring-ioc/auto-wiring/src/test/java/soundsystem/CDPlayerTest.java
@@ -0,0 +1,17 @@
+package soundsystem;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = CDPlayerCongfig.class)
+public class CDPlayerTest {
+
+
+
+}
