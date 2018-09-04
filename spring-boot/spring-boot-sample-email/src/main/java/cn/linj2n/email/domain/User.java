9adad8110ba37458b4bee3813974fcdafe59dd44
linj2n <linj2n@163.com>
2018-09-04 13:13:43 +0800

    Add User class

---
+++ spring-boot/spring-boot-sample-email/src/main/java/cn/linj2n/email/domain/User.java
@@ -0,0 +1,54 @@
+package cn.linj2n.email.domain;
+
+import java.time.ZonedDateTime;
+
+public class User {
+    private String login;
+    private String password;
+    private String email;
+    private String activationKey;
+    private String resetKey;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    public String getActivationKey() {
+        return activationKey;
+    }
+
+    public void setActivationKey(String activationKey) {
+        this.activationKey = activationKey;
+    }
+
+    public String getResetKey() {
+        return resetKey;
+    }
+
+    public void setResetKey(String resetKey) {
+        this.resetKey = resetKey;
+    }
+
+}
