639e4c6ac0f4b641874b115dd00979fe88c06ea2
linj2n <linj2n@163.com>
2018-09-13 23:28:01 +0800

    Update

--- spring-boot/spring-boot-sample-account-verification-module/src/main/resources/import.sql
+++ spring-boot/spring-boot-sample-account-verification-module/src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO ib_authority (id, name) VALUES (1, 'ROLE_ADMIN');
-INSERT INTO ib_authority (id, name) VALUES (2, 'ROLE_USER');
+INSERT INTO vm_authority (id, name) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO vm_authority (id, name) VALUES (2, 'ROLE_USER');
