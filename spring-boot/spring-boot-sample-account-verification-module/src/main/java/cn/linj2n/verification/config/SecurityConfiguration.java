666659872f03b942af19899ecf4dd6f699ca71b6
linj2n <linj2n@163.com>
2018-09-27 23:00:13 +0800

    🚧 Update
    NONE #31

--- spring-boot/spring-boot-sample-account-verification-module/src/main/java/cn/linj2n/verification/config/SecurityConfiguration.java
+++ spring-boot/spring-boot-sample-account-verification-module/src/main/java/cn/linj2n/verification/config/SecurityConfiguration.java
@@ -1,9 +1,13 @@
 package cn.linj2n.verification.config;
 
 import cn.linj2n.verification.repository.UserRepository;
+import cn.linj2n.verification.security.AjaxAuthenticationFailureHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,24 +25,39 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AjaxAuthenticationFailureHandler ajaxAuthenticationFailureHandler;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        auth.authenticationProvider(authenticationProvider);
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/index").permitAll()
+                .antMatchers("/blank").permitAll()
                 .antMatchers("/register").permitAll()
                 .antMatchers("/api/register").permitAll()
-                .antMatchers("/api/activate").permitAll();
+                .antMatchers("/api/activate").permitAll()
+                .antMatchers("/api/account/authentication").permitAll()
+                .antMatchers("/admin/*")
+                .hasAuthority("ROLE_USER")
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/api/account/authentication")
+                .failureHandler(ajaxAuthenticationFailureHandler)
+                .defaultSuccessUrl("/admin/index")
+                .permitAll();
     }
 }
