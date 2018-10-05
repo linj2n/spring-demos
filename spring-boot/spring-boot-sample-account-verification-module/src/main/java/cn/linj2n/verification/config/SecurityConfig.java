880e8bed0af4373bd8ef2b325759aa0364bc7573
linj2n <linj2n@163.com>
2018-10-05 15:37:30 +0800

    🚚 Rename files
    NONE #31

--- spring-boot/spring-boot-sample-account-verification-module/src/main/java/cn/linj2n/verification/config/SecurityConfig.java
+++ spring-boot/spring-boot-sample-account-verification-module/src/main/java/cn/linj2n/verification/config/SecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
