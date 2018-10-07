e818a4c8bdefb738523005c3753accf1afd633b9
linj2n <linj2n@163.com>
2018-10-07 20:59:27 +0800

    🚧 Update database-data for test
    NONE #31

--- spring-boot/spring-boot-sample-account-verification-module/src/main/resources/import.sql
+++ spring-boot/spring-boot-sample-account-verification-module/src/main/resources/import.sql
@@ -1,2 +1,4 @@
 INSERT INTO vm_authority (id, name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO vm_authority (id, name) VALUES (2, 'ROLE_USER');
+INSERT INTO vm_user (activated, activation_key, email, login, password, reset_key, username) VALUES (1, NULL, 'admin@example.com', 'admin', '$2a$10$7minjtSHWO3bDAgaCBzpnelcsE6e6D4ZZ4O.dPqzqdPg5Hxfr..Cu', NULL, 'admin');
+INSERT INTO vm_user_authority (user_id, authority_id) VALUES (1, 1);
