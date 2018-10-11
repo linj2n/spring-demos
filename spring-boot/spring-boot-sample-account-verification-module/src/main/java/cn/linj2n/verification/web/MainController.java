95d24aaee09c1b0b7d4027280d7b39461c585dd1
linj2n <linj2n@163.com>
2018-10-11 23:24:53 +0800

    🚧 Update
    NONE #31

--- spring-boot/spring-boot-sample-account-verification-module/src/main/java/cn/linj2n/verification/web/MainController.java
+++ spring-boot/spring-boot-sample-account-verification-module/src/main/java/cn/linj2n/verification/web/MainController.java
@@ -6,10 +6,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -47,24 +50,31 @@ public class MainController {
     }
 
     @RequestMapping(value = "/account/activate", method = RequestMethod.GET)
-    public String activateAccount(Locale local,@RequestParam(value = "activationKey",required=true) final String activationKey, RedirectAttributes redirectAttrs) {
+    public String activateAccount(Locale local,@RequestParam(value = "key") final String activationKey ,RedirectAttributes attributes) {
         logger.info("Get URL /account/activate with key : {}" + activationKey);
         if (userService.activateRegistration(activationKey).isPresent()) {
-            redirectAttrs.addAttribute("message",messageSource.getMessage("account.activatedSuccessfully",null,local));
+            attributes.addFlashAttribute("message",messageSource.getMessage("account.activatedSuccessfully",null,local));
         } else {
-            redirectAttrs.addAttribute("message",messageSource.getMessage("account.failedToActivate",null,local));
+            attributes.addFlashAttribute("error",messageSource.getMessage("account.failedToActivate",null,local));
         }
         return "redirect:/login";
     }
 
-    @RequestMapping(value = "/account/resetPassword",method = RequestMethod.GET)
-    public String getPasswordResetPage(){
-        return "resetPassword";
+    @RequestMapping(value = "/account/forget_password", method = RequestMethod.GET)
+    public String getResetEmailInputView() {
+        return "/forget_password";
     }
-
-//    @RequestMapping(value = "/account/resetPassword",method = RequestMethod.POST)
-//    public String getPasswordResetPage(){
-//        return "resetPassword";
-//    }
-
+    @RequestMapping(value = "/account/password_reset/{resetKey}", method = RequestMethod.GET)
+    public ModelAndView getPasswordResetView(@PathVariable final String resetKey, ModelMap model, Locale local) {
+        if (!userService.checkIfValidResetPwdKey(resetKey)) {
+            logger.info("Get URL /account/password_reset/{} : Wrong Key.", resetKey);
+            model.addAttribute("error",messageSource.getMessage("account.failedToActivate",null,local));
+            return new ModelAndView("login",model);
+        }
+        return new ModelAndView("/password_reset", model);
+    }
+    @RequestMapping(value = "/account/password_reset/{resetKey}", method = RequestMethod.POST)
+    public String resetPassword(@PathVariable final String resetKey){
+        return null;
+    }
 }
